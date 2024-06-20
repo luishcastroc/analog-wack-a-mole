1479177d0967ba6ed9db0c16eac3d8486181855e
Luis Castro <luish.castroc@gmail.com>
2024-06-20 11:13:42 -0400

    chore: fix style for score and timer

--- game/src/app/components/score.component.ts
+++ game/src/app/components/score.component.ts
@@ -4,11 +4,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   selector: 'game-score',
   standalone: true,
   template: `<img
-      class="absolute left-0 z-[2] h-12 w-12"
+      class="absolute left-0 z-[2] h-10 w-10 md:h-12 md:w-12"
       src="/img/icon_score.png"
       alt="Score icon, a tiny golden coin" />
     <div
-      class="absolute left-[20px] right-[5px] flex h-6 max-w-[8rem] items-center justify-center rounded-[13px] bg-white">
+      class="absolute left-[20px] right-[5px] flex h-6 max-w-[8rem] items-center justify-center rounded-[13px] bg-white text-[0.8rem] md:text-[1rem]">
       {{ score() }}
     </div>`,
   styles: `
--- game/src/app/components/timer.component.ts
+++ game/src/app/components/timer.component.ts
@@ -6,9 +6,9 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
   template: `<img
       src="/img/icon_time.png"
       alt="Time icon, a tiny clock"
-      class="absolute left-0 z-[2] h-12 w-12" />
+      class="absolute left-0 z-[2] h-10 w-10 md:h-12 md:w-12" />
     <div
-      class="absolute left-[20px] right-[5px] flex h-6 max-w-[8rem] items-center justify-center rounded-[13px] bg-white">
+      class="absolute left-[20px] right-[5px] flex h-6 max-w-[8rem] items-center justify-center rounded-[13px] bg-white text-[0.8rem] md:text-[1rem]">
       {{ time() }}
     </div>`,
   styles: `
